fec7fa61e30270c50ed715fd52d05ccba734e1b1
kricha22 <richadwivedi578@gmail.com>
2026-02-21 22:12:34 +0530

    logger

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,20 +1,28 @@
 package tests;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class HomePageTest extends Base{
+    private static final Logger logger = LogManager.getLogger(HomePageTest.class);
     @Test
     public void validatePortOfCalls() {
         loginPage.navigateToSecondaryLoginPage();
+        logger.info("application launched .....");
         loginPage.enterBookingCredentialsAndLogin();
+        logger.info("logged into application .....");
         homePage.isPortOfCallsPresent();
+        logger.info("port of calls displayed .....");
         homePage.isSeeAllPresent();
         homePage.clickSeeAllCta();
         homePage.validateSeeAllBehavior();
+        logger.info("see all validated .....");
         homePage.selectAnyPort();
         assertAll(homePage.validatePort());
+        logger.info("ui validated .....");
     }
 
   //  @Test
